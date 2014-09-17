ec3a5cde777f5c6ebe0bbc49949aab9c054c63de
Juho Nissi <juho_356@hotmail.com>
2014-09-17 13:49:02 +0300

    Harjoitus 3

---
+++ harj3a.cpp
@@ -0,0 +1,11 @@
+/*Kommentti alkaa
+ja jatkuu
+ja jatkuu ... */
+#include <iostream> // otsikkotiedosto sis‰llytet‰‰n ohjelmaan
+using namespace std;
+void main(void) // p‰‰ohjelma alkaa
+{
+	cout << "Juho Nissi\n"
+		<< "Palstatie 4 A 7\n"
+		<< "40520 Jyvaskyla\n";
+}
---
+++ harj3b.cpp
@@ -0,0 +1,16 @@
+/*Kommentti alkaa
+ja jatkuu
+ja jatkuu ... */
+#include <iostream> // otsikkotiedosto sis‰llytet‰‰n ohjelmaan
+using namespace std;
+void main(void) // p‰‰ohjelma alkaa
+{
+	cout << "********************\n""* *\n" "* Juho\n"
+		<< "* Nissi\n" "* *\n";
+	cout << "*-------------------\n"
+		<< "* *\n"
+		<< "* Palstatie 4 A 7\n"
+		<< "* 40520 Jyvaskyla\n"
+		<< "* *\n"
+		<< "********************\n";
+}
